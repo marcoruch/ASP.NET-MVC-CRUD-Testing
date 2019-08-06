5644ea07cdf0d2babda7e2563c8ae333de7e5cb4
marcoruch <marco_ruch@gmx.ch>
2019-08-06 16:05:39 +0200

    Created Person Views, Model, Controller
    1. Controller
    # Person Controller
    
    2. Model
    # PersonModel
    
    3. Views
    
    Co-Authored-By: Marco Ruch <marco_ruch@gmx.ch>
    # Index
    # ListPeople

---
+++ CRUD-Testing/Controllers/PersonController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_Testing.Models;
+
+namespace CRUD_Testing.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            var people = new List<PersonModel>
+            {
+                new PersonModel()
+                {
+                    Id = 1,
+                    FirstName = "Marco",
+                    LastName = "Ruch",
+                    Age = 15,
+                    IsAlive = true,
+                },
+                new PersonModel()
+                {
+                    Id = 3,
+                    FirstName = "Thierry",
+                    LastName = "Mooser",
+                    Age = 29,
+                    IsAlive = true,
+                },
+                new PersonModel()
+                {
+                    Id = 1,
+                    FirstName = "Fabienne",
+                    LastName = "Schlüdele",
+                    Age = 11,
+                    IsAlive = false,
+                }
+            };
+
+
+
+
+            return View(people);
+        }
+    }
+}
---
+++ CRUD-Testing/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_Testing.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
